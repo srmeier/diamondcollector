315c0fd8f74b7dedaa014bc96a8e72acdfbc0319
Stephen Meier <info@stephenmeier.net>
2014-12-22 16:12:12 -0500

    adding base files

---
+++ client/main.c
@@ -0,0 +1,18 @@
+/*
+gcc main.c -o client.exe -I./include -L./lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_net
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_net.h>
+
+//-----------------------------------------------------------------------------
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
